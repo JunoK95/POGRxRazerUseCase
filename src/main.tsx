3b31eaeec498327ab33638f604e5e1fd1f8bcad6
JunoK95 <junokimemail@gmail.com>
2025-08-04 15:41:36 -0600

    Change browserrouter to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import CaseOnePage from "./pages/case/1/CaseOnePage.tsx";
 import CaseTwoPage from "./pages/case/2/CaseTwoPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/case-one" element={<CaseOnePage />} />
         <Route path="/case-two" element={<CaseTwoPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
