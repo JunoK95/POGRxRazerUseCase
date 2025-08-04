4c9dcffc1177076fd4629529170821c24f5630c6
JunoK95 <junokimemail@gmail.com>
2025-08-04 16:22:36 -0600

    change src of iframes

--- src/pages/case/1/CaseOnePage.tsx
+++ src/pages/case/1/CaseOnePage.tsx
@@ -2,62 +2,62 @@ function CaseOnePage() {
   return (
     <div style={{ width: "100%", border: "none" }}>
       <iframe
-        src="/case-one/pogr-razer-1-1.html"
+        src="/POGRxRazerUseCase/case-one/pogr-razer-1-1.html "
         title="Page 1"
         style={{ width: "100%", height: "105vh", border: "none" }}
       />
       <iframe
-        src="/case-one/pogr-razer-1-2.html"
+        src="/POGRxRazerUseCase/case-one/pogr-razer-1-2.html"
         title="Page 2"
         style={{ width: "100%", height: "105vh", border: "none" }}
       />
       <iframe
-        src="/case-one/pogr-razer-1-3.html"
+        src="/POGRxRazerUseCase/case-one/pogr-razer-1-3.html"
         title="Page 3"
         style={{ width: "100%", height: "105vh", border: "none" }}
       />
       <iframe
-        src="/case-one/pogr-razer-1-4.html"
+        src="/POGRxRazerUseCase/case-one/pogr-razer-1-4.html"
         title="Page 4"
         style={{ width: "100%", height: "110vh", border: "none" }}
       />
       <iframe
-        src="/case-one/pogr-razer-1-5.html"
+        src="/POGRxRazerUseCase/case-one/pogr-razer-1-5.html"
         title="Page 5"
         style={{ width: "100%", height: "105vh", border: "none" }}
       />
       <iframe
-        src="/case-one/pogr-razer-1-6.html"
+        src="/POGRxRazerUseCase/case-one/pogr-razer-1-6.html"
         title="Page 6"
         style={{ width: "100%", height: "105vh", border: "none" }}
       />
       <iframe
-        src="/case-one/pogr-razer-1-7.html"
+        src="/POGRxRazerUseCase/case-one/pogr-razer-1-7.html"
         title="Page 7"
         style={{ width: "100%", height: "110vh", border: "none" }}
       />
       <iframe
-        src="/case-one/pogr-razer-1-8.html"
+        src="/POGRxRazerUseCase/case-one/pogr-razer-1-8.html"
         title="Page 8"
         style={{ width: "100%", height: "105vh", border: "none" }}
       />
       <iframe
-        src="/case-one/pogr-razer-1-9.html"
+        src="/POGRxRazerUseCase/case-one/pogr-razer-1-9.html"
         title="Page 9"
         style={{ width: "100%", height: "105vh", border: "none" }}
       />
       <iframe
-        src="/case-one/pogr-razer-1-10.html"
+        src="/POGRxRazerUseCase/case-one/pogr-razer-1-10.html"
         title="Page 10"
         style={{ width: "100%", height: "105vh", border: "none" }}
       />
       <iframe
-        src="/case-one/pogr-razer-1-12.html"
+        src="/POGRxRazerUseCase/case-one/pogr-razer-1-12.html"
         title="Page 12"
         style={{ width: "100%", height: "125vh", border: "none" }}
       />
       <iframe
-        src="/case-one/pogr-razer-1-13.html"
+        src="/POGRxRazerUseCase/case-one/pogr-razer-1-13.html"
         title="Page 13"
         style={{ width: "100%", height: "105vh", border: "none" }}
       />
