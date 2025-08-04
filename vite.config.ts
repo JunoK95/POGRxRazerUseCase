9b47fb81477412b0fc3476c57ca353326faed0a4
JunoK95 <junokimemail@gmail.com>
2025-08-04 15:36:14 -0600

    Add ghpages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+const repoName = "POGRxRazerUseCase";
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: `/${repoName}/`, // Required for GitHub Pages
+});
