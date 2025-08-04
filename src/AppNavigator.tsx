d5ea7944552760a05dc407d0af95101bd7198929
JunoK95 <junokimemail@gmail.com>
2025-08-04 16:11:24 -0600

    Add location and key props

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -1,11 +1,12 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation } from "react-router";
 import App from "./App";
 import CaseOnePage from "./pages/case/1/CaseOnePage";
 import CaseTwoPage from "./pages/case/2/CaseTwoPage";
 
 function AppNavigator() {
+  const location = useLocation();
   return (
-    <Routes>
+    <Routes location={location} key={location.pathname}>
       <Route path="/" element={<App />} />
       <Route path="/case-one" element={<CaseOnePage />} />
       <Route path="/case-two" element={<CaseTwoPage />} />
