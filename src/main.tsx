f8d24ef2abcd30f747bd501c5422d2ed2fa469d0
JunoK95 <junokimemail@gmail.com>
2025-08-04 15:55:08 -0600

    Change to browserrouter and add basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { HashRouter } from "react-router";
+import { BrowserRouter } from "react-router";
 import AppNavigator from "./AppNavigator.tsx";
+
+const repoName = "POGRxRazerUseCase";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={`/${repoName}`}>
       <AppNavigator />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
